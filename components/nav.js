576358d96762e678aa89d3c83d7b56340345ecfd
Iulian <savin.iulian90@gmail.com>
2015-10-07 14:31:29 +0300

    added header

--- components/nav.js
+++ components/nav.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-
 import styles from './nav.css';
-
+// <nav className={styles.nav}>
+//           <Link to='/'>Home</Link>
+//           <Link to='/about'>About</Link>
+//         </nav>
 export default class Nav extends Component {
   render() {
     return (
-      <nav className={styles.nav}>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-      </nav>
+      <div>
+        <div className={'mdl-layout mdl-js-layout mdl-layout--fixed-header' + styles.test}>
+          <header className="mdl-layout__header">
+            <div className="mdl-layout__header-row">
+              <img src='http://learnfwd.com/assets/img/logo-learnfwd-03.png' className={styles.logoHeader}></img>
+              <span className="mdl-layout-title">Learn Forward</span>
+              <div className="mdl-layout-spacer"></div>
+              <nav className="mdl-navigation mdl-layout--large-screen-only">
+                <Link className="mdl-navigation__link" to="/">Home</Link>
+                <Link className="mdl-navigation__link" to="/about">About</Link>
+                <Link className="mdl-navigation__link" to="/demo">Demo</Link>
+                <Link className="mdl-navigation__link" to="/team">Team</Link>
+                <Link className="mdl-navigation__link" to="/contact">Contact</Link>
+              </nav>
+            </div>
+          </header>
+          <div className="mdl-layout__drawer">
+            <span className="mdl-layout-title">Learn Forward</span>
+            <nav className="mdl-navigation">
+              <Link className="mdl-navigation__link" to="/">Home</Link>
+              <Link className="mdl-navigation__link" to="/about">About</Link>
+              <Link className="mdl-navigation__link" to="/demo">Demo</Link>
+              <Link className="mdl-navigation__link" to="/team">Team</Link>
+              <Link className="mdl-navigation__link" to="/contact">Contact</Link>
+            </nav>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- pages/home.js
+++ pages/home.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 export default class Page extends Component {
   render() {
     return (
-      <h1>
-      HomePage
-      </h1>
+      <div>
+        <h1>
+        HomePage
+        </h1>
+      </div>
     );
   }
 }
