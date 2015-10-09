fbd5b3be8439f1b8e7565d6308dbf76bb978f6c4
Leo Budrinca <leo@readfwd.com>
2015-10-09 18:13:02 +0300

    Add content to lfa, lfp pages

--- pages/demo.js
+++ pages/demo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import styles from './demo.css';
 
 export default class Page extends Component {
@@ -8,6 +9,20 @@ export default class Page extends Component {
         <h1 className={styles.example}>
           Demo
         </h1>
+        <h3>Features not to miss in the demo:
+        </h3>
+        <ul>
+          <li>Change your automatically assigned name to your real name</li>
+          <li>Check out all the great exercises (try to solve them, they are real exercises)</li>
+          <li>Create a classroom (you became a teachet this way)</li>
+          <li>Get another computer to access the same <a href="http://demo.lfwd.io" target="_blank">demo.lfwd.io</a> and join the classroom you created before (you become a student)</li>
+          <li>If you feel adventurous get another tablet or smartphone and connect to the same classroom (another student)</li>
+          <li>Select a group from both students (A, let's say) and solve exercises together</li>
+          <li>Fun, right? If you liked it (or not) drop us an email at <a href="hello@learnfwd.com?Subject=Hello%20again" target="_top">hello@learnfwd.com</a></li>
+        </ul>
+        <div className='text-center'>
+        <button type="button" className="btn btn-danger navbar-btn"><a href="http://demo.lfwd.io" target="_blank">Open demo</a></button>
+        </div>
       </div>
     );
   }
--- pages/lfa.js
+++ pages/lfa.js
@@ -6,8 +6,48 @@ export default class Page extends Component {
     return (
       <div>
         <h1 className={styles.background}>
-          Learn Forward Author (LFA)
+          Learn Forward Author (LFA) - tool for everyone to try
         </h1>
+        <p>
+          Our core technology for creating advanced html5 digital textbooks. We keep it open-source and totally free for everybody to use. It is a command-line tool that help you build digital textbook that you can open in your own browser, no server-side or other app needed. Technologies used: HTML5, CSS, Javascript, node.js.
+        </p>
+        <h3>
+          Free to use! <a href="https://github.com/learnfwd/lfa" target="_blank">See on github</a>
+        </h3>
+        <ul>
+          <li>no reader required: your browser is the best reader in the world</li>
+          <li>built using open standards: HTML5, CSS, Javascript, Node.js. In fact LearnFwd Author is a node package (try `npm install -g lfa`).</li>
+          <li>super-fast command-line tool that uses mainly HTML (jade precompiler) and CSS (stylus precompiler) to produce clean HTML5 textbooks</li>
+          <li>works both offline (local folder, CD) and online (on all kinds of web servers)</li>
+          <li>automatically saves user's progress in local storage (all results to exercises, all notes, etc.)</li>
+        </ul>
+      <h1 className={styles.background}>
+        LearnFwd Classroom
+      </h1>
+      <h3>
+        Add powerful exercises and collaborative features for in-classroom use
+      </h3>
+      <p>
+        Built upon LearnFwd Author but featuring advanced plugins for automatic backup of user’s progress, creating and using complex self-validating exercises and real-time collaborative features tailored for in-classroom use (create / join classrooms, work in groups, share results etc.). Collaborative features require internet connection.
+      </p>
+      <h3>
+        10+ types of exercises
+      </h3>
+      <ul>
+        <li>"pick one" (single choice)</li>
+        <li>"select to highlight" (multiple-choice on all kinds of elements: images, text, buttons)</li>
+        <li>"yes/no" (chose whether the statement is true or false)</li>
+        <li>"free drawing pad" (automatically saved)</li>
+        <li>"validated textline" (free writing areas validated with regular expressions)</li>
+        <li>"drag and sort" (arrange items in the correct order: images, lists, boxes, etc.)</li>
+        <li>"drag to bucket" (group items that satisfy a certain criterion)</li>
+        <li>"split words/sentences" (split a string of text: e.g. split in syllables, add correct punctuation marks, etc.)</li>
+        <li>"combine elements" (e.g. combine syllables in words, words in phrases, etc.; validate results based on provided dictionary)</li>
+        <li>"connect points" (make the right connections; e.g. labels to photos)</li>
+        <li>"arithmetic exercises" (validate results using equations)</li>
+      </ul>
+
+
       </div>
     );
   }
