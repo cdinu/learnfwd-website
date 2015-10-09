c9542b9e2cb4f754d6b566c0c7e14a6ccc51530b
Iulian <savin.iulian90@gmail.com>
2015-10-09 15:27:18 +0300

    work on header

--- components/nav.js
+++ components/nav.js
@@ -25,6 +25,11 @@ export default class Nav extends Component {
             </div>
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav navbar-right">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/">Products + Features</Link></li>
+                <li><Link to="/solutions">Solutions for</Link></li>
+                <li><Link to="/demo">Demo</Link></li>
+                <li><Link to="/about">About us</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
                 <li className="dropdown">
                   <a href="#" className="dropdown-toggle" dataToggle="dropdown" role="button" ariaHaspopup="true" ariaExpanded="false">Dropdown <span className="caret"></span></a>
