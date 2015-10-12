41a3f5d7f9a68926d98e8c94ba03293d69e4f431
Leo Budrinca <leo@readfwd.com>
2015-10-12 14:51:53 +0300

    Add content

--- pages/publishers1.js
+++ pages/publishers1.js
@@ -3,66 +3,46 @@ import React, { Component } from 'react';
 import styles from './publishers1.css';
 import Image from '../components/image';
 
-styles.front = 'mdl-grid ' + styles.front;
-styles.frontImage = 'mdl-cell mdl-cell--6-col ' + styles.frontImage;
 
-styles.evolving = 'mdl-grid ' + styles.evolving;
-styles.evolvingLeft = 'mdl-cell mdl-cell--7-col ' + styles.evolvingLeft;
-styles.evolvingRight = 'mdl-cell mdl-cell--5-col ' + styles.evolvingRight;
+styles.fluid = 'mdl-grid ' + styles.fluid;
+styles.fluidLeft = 'mdl-cell mdl-cell--5-col ' + styles.fluidLeft;
+styles.fluidRight = 'mdl-cell mdl-cell--7-col ' + styles.fluidRight;
+
+styles.collaborative = 'mdl-grid ' + styles.collaborative;
+styles.collaborativeLeft = 'mdl-cell mdl-cell--6-col ' + styles.collaborativeLeft;
+styles.collaborativeRight = 'mdl-cell mdl-cell--6-col ' + styles.collaborativeRight;
 
 export default class Page extends Component {
   render() {
     return (
-      <div className={styles.publishers}>
-        <div className={styles.front}>
-          <div className={styles.frontImage}>
-            <Image src='/img/solutions-for-educational-publishers.png'/>
+      <div className={styles.publishers1}>
+        <div className={styles.fluid}>
+          <div className={styles.fluidImageLeft}>
+            <Image src='/img/publishers-fluid-image.png'/>
           </div>
-          <h1>Solutions for Educational Publishers</h1>
-          <h2>You have the content.</h2>
-          <h2>We add digitisation services, real-time technology and proven expertise.</h2>
-          <h2>Together we create the best digital textbooks.</h2>
-        </div>
-        <div className={styles.evolving}>
-          <div className={styles.evolvingLeft}>
-            <h1>Textbooks are evolving.</h1>
-            <h2>PDFs & ePubs are not genuine digital textbooks and your customers know that.</h2>
-          </div>
-          <div className={styles.evolvingRight}>
-            <div className={styles.evolvingRightElementContainer}>
-              <div className={styles.evolvingRightElement}>
-                <Image src='/img/pdf-icon.png' />
-              </div>
-              <div className={styles.evolvingRightElement}>
-                <Image src='/img/epub-icon.png' />
-              </div>
-              <div className={styles.evolvingRightElement}>
-                <Image src='/img/lfa-icon.png' />
-              </div>
-            </div>
-            <div className={styles.evolvingRightElementContainer}>
-              <div className={styles.evolvingRightElementBottom}>
-                <span>static layout</span>
-              </div>
-              <div className={styles.evolvingRightElementBottom}>
-                no digital exercises only fit for individual studying
-              </div>
-              <div className={styles.evolvingRightElementBottom}>
-                collaborative classroom-ready context-aware
-              </div>
-            </div>
+          <div className={styles.fluidContainerRight}>
+            <Image src='/img/publishers-fluid-layout.png'/>
+            <h1>Fluid Layout</h1>
+            <h2>Your content can retain 90% of the print layout.</h2>
+            <h2>We add digitisation services, real-time technology and proven expertise.</h2>
+            <h2>And still adapt for all screen sizes.</h2>
           </div>
         </div>
-        <div className={styles.front}>
-          &nbsp;
+        <div className={styles.collaborative}>
+          <div className={styles.collaborativeLeft}>
+            <h1>Collaborative</h1>
+            <h2>Education is a social process.</h2>
+            <h2> We create textbooks that benefit from technologies, making them fit for modern classrooms where people learn together.</h2>
+          </div>
+          <div className={styles.collaborativeRight}>
+            <Image src='/img/publishers-collaborative.png' />
+          </div>
         </div>
-
-
       </div>
     );
   }
 }
 
-Page.displayName = 'LfPublishers';
+Page.displayName = 'LfPublishers1';
 
 
