5a2f8291a142c8aaa62a133ad8c37839fc98a17f
Iulian <savin.iulian90@gmail.com>
2015-10-12 21:52:33 +0300

    added team page+ necessary images

---
+++ pages/team.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './team.css';
+
+export default class Page extends Component {
+  render() {
+    return (
+      <div className={styles.teamCt}>
+      </div>
+    );
+  }
+}
+
+Page.displayName = 'LfTeam';
+
+
