8eb4a5a50b50920bf52fd7134a8c7aa5204afb98
Mara Predescu <predescu_mara@yahoo.com>
2015-10-12 18:14:50 +0300

    schools2 3/3

--- pages/schools2.js
+++ pages/schools2.js
@@ -4,10 +4,16 @@ import styles from './schools2.css';
 import Image from '../components/image';
 styles.builtCR = 'mdl-grid ' + styles.builtCR;
 styles.onOff = 'mdl-grid ' + styles.onOff;
-styles.builtCRLeft = 'mdl-cell mdl-cell--6-col mdl-cell--middle ' + styles.builtCRLeft;
-styles.builtCRRight = 'mdl-cell mdl-cell--6-col mdl-cell--middle ' + styles.builtCRRight;
-styles.onOffLeft = 'mdl-cell mdl-cell--7-col mdl-cell--middle ' + styles.onOffLeft;
-styles.onOffRight = 'mdl-cell mdl-cell--5-col mdl-cell--middle ' + styles.onOffRight;
+styles.controlClass = 'mdl-grid ' + styles.controlClass;
+
+styles.builtCRLeft = 'mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-cell--middle ' + styles.builtCRLeft;
+styles.builtCRRight = 'mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-cell--middle ' + styles.builtCRRight;
+
+styles.onOffLeft = 'mdl-cell mdl-cell--7-col mdl-cell--8-col-tablet mdl-cell--middle ' + styles.onOffLeft;
+styles.onOffRight = 'mdl-cell mdl-cell--5-col mdl-cell--8-col-tablet mdl-cell--middle ' + styles.onOffRight;
+
+styles.controlClassLeft = 'mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-cell--middle ' + styles.controlClassLeft;
+styles.controlClassRight = 'mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-cell--middle ' + styles.controlClassRight;
 export default class Page extends Component {
   render() {
     return (
@@ -18,7 +24,7 @@ export default class Page extends Component {
             <h2>Education is a collaborative process and students are able to work in groups when solving the quizzes you added to your lessons.</h2>
           </div>
           <div className={styles.builtCRRight}>
-            <Image src='/img/schools-collaborative@2x.png'/>
+            <Image src='/img/schools-collaborative.png'/>
           </div>
         </div>
         <div className={styles.onOff}>
@@ -29,7 +35,18 @@ export default class Page extends Component {
             <h1>Online & Offline</h1>
             <h2>What if the internet fails?</h2>
             <br></br>
-            <h2>You can keep teaching your lesson: the lessons and all solved exercises are automatically saved in everyone’s browser.</h2>  
+            <h2>You can keep teaching your lesson: the lessons and all solved exercises are automatically saved in everyone’s browser.</h2>
+          </div>
+        </div>
+        <div className={styles.controlClass}>
+          <div className={styles.controlClassLeft}>
+            <h1>Control your class</h1>
+            <h2>Start a virtual classroom from your own lesson. No sign-up for you or your students required. See who’s paying attention (or not).</h2>
+            <br></br>
+            <h2>The simplest tool you will find.</h2>
+          </div>
+          <div className={styles.controlClassRight}>
+            <Image src='/img/schools-control-your-class.png'/>
           </div>
         </div>
       </div>
