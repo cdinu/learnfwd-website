ee50f8c949c1e2e39eb9a472d0ac05510157fff7
Cristian Dinu <cristian.dinu@learnfwd.com>
2015-10-12 21:55:46 +0100

    Fixes on footer

--- components/footer.js
+++ components/footer.js
@@ -15,21 +15,24 @@ export default class Footer extends Component {
         </div>
         <div className={sectionStyle}>
           <strong className='sectionTitle'>Solutions</strong>
-          <Link className={styles.sectionLink} to='lfa'>For Educational Publishers</Link>
-          <Link className={styles.sectionLink} to='lfa'>For Teachers &amp; Schools</Link>
-          <Link className={styles.sectionLink} to='lfa'>For Coroprate Training</Link>
+          <Link className={styles.sectionLink} to='/publishers'>For Educational Publishers</Link>
+          <Link className={styles.sectionLink} to='/schools'>For Teachers &amp; Schools</Link>
+          <Link className={styles.sectionLink} to='/corporate'>For Coroprate Training</Link>
         </div>
         <div className={sectionStyle}>
           <strong className='sectionTitle'>Company</strong>
-          <Link className={styles.sectionLink} to='lfa'>Team & Advisors</Link>
+          <Link className={styles.sectionLink} to='/team'>Team & Advisors</Link>
+          <Link className={styles.sectionLink} to='/contact'>Contact Us</Link>
+          {/*
           <Link className={styles.sectionLink} to='lfa'>Press</Link>
           <Link className={styles.sectionLink} to='lfa'>Jobs</Link>
           <Link className={styles.sectionLink} to='lfa'>Investors</Link>
+           */}
         </div>
         <div className={sectionStyle}>
           <strong className='sectionTitle'>Learn Forward Limited</strong>
           <span className={styles.sectionLink} to='lfa'>Registered in England and Wales</span>
-          <span className={styles.sectionLink} to='lfa'>Register nr. 9401061</span>
+          <a className={styles.sectionLink} href='https://beta.companieshouse.gov.uk/company/09401061' target='_blank'>Register nr. 9401061</a>
           <a className={styles.sectionLink} href='http://ec.europa.eu/taxation_customs/vies/viesquer.do?ms=GB&iso=GB&vat=216729891' target='_blank'>VAT nr. GB&thinsp;216729891</a>
           <a className={styles.sectionLink} href='mai&#108;t&#111;&#58;hello&#64;%&#55;&#50;&#37;&#54;&#53;a%&#54;4%66w&#37;64%2Ec%6Fm'>&#104;&#101;&#108;&#108;&#111;&#64;re&#97;&#100;fwd&#46;&#99;om</a>
         </div>
