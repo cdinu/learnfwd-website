6ae5e4f79a5f9f3d6c1765470ca7e1cfad5c4340
Iulian <savin.iulian90@gmail.com>
2015-10-13 20:47:45 +0300

    work on button-modal/demo-modal components

--- pages/404.js
+++ pages/404.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './404.css';
 import Image from '../components/image';
 import { Link } from 'react-router';
+import DemoButton from '../components/demo-button.js';
 
 export default class Page extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Page extends Component {
         <Image src='/img/404.png'/>
         <h4>The page you&apos;re trying to reach doesn&apos;t exist.</h4>
         <p>Go back to <Link to='/'>learnfwd.com</Link> or <Link to='/contact'>contact us</Link> about the problem.</p>
+        <DemoButton>ceva</DemoButton>
       </div>
     );
   }
