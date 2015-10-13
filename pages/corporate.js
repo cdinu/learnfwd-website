43175e366ebb45f79c2e8f389576ba4edfea77cf
Iulian <savin.iulian90@gmail.com>
2015-10-13 21:02:33 +0300

    added corporate page + removed unnecessary pages

---
+++ pages/corporate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './corporate.css';
+// import Image from '../components/image';
+// import { Link } from 'react-router';
+export default class Page extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1>to do</h1>
+      </div>
+    );
+  }
+}
+
+Page.displayName = 'LfpCorporate';
--- routes.js
+++ routes.js
@@ -80,17 +80,14 @@ const meta = {
 export default (
   <Route component={app} menu={menu} meta={meta} path='/'>
     <IndexRoute component={pages.home} />
-    <Route component={pages.about} path='about' />
-    <Route component={pages.contact} path='contact' />
-    <Route component={pages.lfa} path='lfa' />
-    <Route component={pages.lfp} path='lfp' />
-    <Route component={pages.products} path='products' />
     <Route component={pages.publishers} path='publishers' />
     <Route component={pages.schools} path='schools' />
-    <Route component={pages.solutions} path='solutions' />
+    <Route component={pages.corporate} path='corporate' />
+    <Route component={pages.lfa} path='lfa' />
+    <Route component={pages.lfp} path='lfp' />
     <Route component={pages.team} path='team' />
+    <Route component={pages.contact} path='contact' />
     <Route component={pages.demo} path='demo' />
     <Route component={pages.notFound} path="*" />
   </Route>
 );
-
