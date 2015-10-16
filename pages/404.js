98907a5c16ba6ec21e52d197613e2185e64c0530
Iulian <savin.iulian90@gmail.com>
2015-10-16 11:30:50 +0300

    cleaned 404 page

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import styles from './404.css';
 import Image from '../components/image';
 import { Link } from 'react-router';
-import DemoButton from '../components/demo-button.js';
 
 export default class Page extends Component {
   render() {
@@ -11,7 +10,6 @@ export default class Page extends Component {
         <Image src='/img/404.png'/>
         <h4>The page you&apos;re trying to reach doesn&apos;t exist.</h4>
         <p>Go back to <Link to='/'>learnfwd.com</Link> or <Link to='/contact'>contact us</Link> about the problem.</p>
-        // <DemoButton>ceva</DemoButton>
       </div>
     );
   }
