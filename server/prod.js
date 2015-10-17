482f274a366da66f42fa9a80727dbebba141a32b
Paul Balogh <paul.balogh@gmail.com>
2015-10-17 21:20:37 +0200

    add sitemap.xml

--- server/prod.js
+++ server/prod.js
@@ -3,15 +3,47 @@ import { RoutingContext, match } from 'react-router';
 import { createMemoryHistory } from 'history';
 import fs from 'fs';
 import express from 'express';
+import sm from 'sitemap';
 
 import routes from '../routes';
 
 const app = express();
 
+const urls = [];
+
+function sh(sub) {
+  if (sub.items) {
+    sub.items.forEach(sh);
+  } else {
+    urls.push({
+      url: sub.to,
+      changefreq: 'daily',
+    });
+  }
+}
+
+routes.props.menu.forEach(menuItem => sh(menuItem) );
+
+
 const template = fs.readFileSync('./server-dist/index.html').toString();
 const history = createMemoryHistory();
+const sitemap = sm.createSitemap({
+  hostname: 'http://learnfwd.com',
+  cacheTime: 600000,
+  urls: urls,
+});
 
-app.use(express.static('./dist', {index: false }));
+app.use(express.static('./dist', {index: false}));
+
+app.get('/sitemap.xml', (req, res) => {
+  sitemap.toXML(function(err, xml) {
+    if (err) {
+      return res.status(500).end();
+    }
+    res.header('Content-Type', 'application/xml');
+    res.send(xml);
+  });
+});
 
 app.get('*', (req, res) => {
   const location = history.createLocation(req.url);
@@ -30,7 +62,7 @@ app.get('*', (req, res) => {
   });
 });
 
-const server = app.listen(process.env.PORT || 5392 , () => {
+const server = app.listen(process.env.PORT || 5392, () => {
   const { host, port } = server.address();
   console.log('App listening at http://%s:%s', host || 'localhost', port);
 });
