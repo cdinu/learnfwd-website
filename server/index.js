c88048a76f35560d1d1715b5881fb9970e5ce317
Cristian Dinu <cristian.dinu@learnfwd.com>
2015-10-27 15:33:58 +0000

    Add production/dev console.log message

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-
 if (global.DEBUG) {
+  console.log('Starting in develpment mode.');
   require('./dev');
 } else {
+  console.log('Production environment detected.');
   require('newrelic');
   require('./prod');
 }
