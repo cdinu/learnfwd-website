43817a04b8e8f87e019aa315a4f1ce23e643727b
Leo Budrinca <leo@readfwd.com>
2017-01-23 13:38:05 +0200

    Add content to hypersay

--- pages/lfp.js
+++ pages/lfp.js
@@ -12,15 +12,20 @@ export default class Page extends Component {
       <div>
         <div className={styles.lfpFirst} style={style}>
           <h1>
-            <span className={styles.faded}>◉</span>Platform
+            <span className={styles.faded}>◉</span>Hypersay
           </h1>
-          <h2>Digital lesson planning & </h2>
-          <h2>delivery tool.</h2>
-          <h2>With a twist.</h2>
+          <h2>Create beautiful and responsive presentations engaging the audience in real time</h2>
+          <h2> </h2>
         </div>
         <div className={styles.lfaSecond}>
           <div className={styles.maxW}>
-            <p>We are currently working on a platform that allows teachers to create, adapt and deliver lessons in a very straightforward manner.</p>
+            <p><strong>Simply.</strong> Drag & drop your powerpoint presentation @ www.hypersay.com. </p>
+            <p><strong>Quick.</strong> Add interactions: quize, text question, text section (markdown support), YouTube videos.</p>
+            <p><strong>Responsive. </strong>Connect to your presentation on any browser on any device.</p>
+            <p><strong>Secure. </strong>All media that you upload is available in your personal media library.</p>
+            <p><strong>Real-time. </strong>Get instant feedback from your audience.</p>
+            <p><strong>Polls. </strong>Create beautiful interactive polls in the browser.</p>
+            <p><strong>Votes. </strong>Vote casting likes with an awesome like counter.</p>
           </div>
         </div>
         <div className={styles.lfaSeventh}>
